a32e57e7eee2c357a19b5e35bde59d4338dd694e
Steex01 <sedsonsteph@gmail.com>
2018-05-22 22:00:05 +0200

    oo

--- application/config/database.php
+++ application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-    'dsn'	=> 'pgsql:host=localhost;dbname=teleoperateur;port=5432',
+    'dsn'	=> 'mysql:host=localhost;port=3307;dbname=planeair',
     'hostname' => 'localhost',
-    'username' => 'postgres',
-    'password' => 'Antsalol',
-    'database' => 'teleoperateur',
-    'dbdriver' => 'pdo',
+    'username' => 'root',
+    'password' => 'root',
+    'database' => 'planeair',
+    'dbdriver' => 'mysqli',
     'dbprefix' => '',
     'pconnect' => FALSE,
     'db_debug' => FALSE,
--- application/views/login.php
+++ application/views/login.php
@@ -26,48 +26,7 @@
       <a class="hiddenanchor" id="signup"></a>
       <a class="hiddenanchor" id="signin"></a>
 
-      <div class="login_wrapper">
-        <div class="animate form login_form">
-          <section class="login_content">
-            <?php echo form_open(base_url().'Utilisateur',array('method'=>'post'));?>
-            <h1>TELEOPERATEUR</h1>
-            <input type="hidden"  name="table" value="agent"/>
-              <div>
-                <input type="email" class="form-control" placeholder="Adresse email" name="mail" required="" />
-                  <?php echo form_error('mail'); ?>
-              </div>
-              <div>
-                <input type="password" class="form-control" placeholder="Mot de passe" name="pass" required="" />
-                  <?php echo form_error('pass'); ?>
-              </div>
-              <?php if ($error!=""){ ?>
-                <div class="alert alert-danger">
-                  <?php echo $error; ?>
-                </div>
-              <?php } ?>
-              <div>
-                <input type="submit" class="btn btn-default submit" value="Je me connecte">
-                <a class="reset_pass" href="#">Lost your password?</a>
-              </div>
-
-              <div class="clearfix"></div>
-
-              <div class="separator">
-                <p class="change_link">Etes-vous un Manager?
-                  <a href="#signup" class="to_register"> Connectez-vous ici! </a>
-                </p>
-
-                <div class="clearfix"></div>
-                <br />
-
-                <div>
-                  <h1><i class="fa fa-paw"></i> TeleOperateur!</h1>
-                  <p>©2017 All Rights Reserved. TeleOperateur.</p>
-                </div>
-              </div>
-            <?php echo form_close(); ?>
-          </section>
-        </div>
+    </div>
 
         <div id="register" class="animate form registration_form">
           <section class="login_content">
@@ -89,7 +48,7 @@
             <?php } ?>
             <div>
               <input type="submit" class="btn btn-default submit" value="Je me connecte">
-              <a class="reset_pass" href="#">Lost your password?</a>
+              <a class="reset_pass" href="#">Vous avez perdu votre mots de passe ?</a>
             </div>
 
             <div class="clearfix"></div>
@@ -103,8 +62,8 @@
               <br />
 
               <div>
-                <h1><i class="fa fa-paw"></i> TeleOperateur!</h1>
-                <p>©2017 All Rights Reserved. TeleOperateur.</p>
+                <h1><i class="fa fa-paw"></i> Plane'R</h1>
+                <p>©2018 All Rights Reserved .Plane'R.</p>
               </div>
             </div>
             <?php echo form_close(); ?>
