60960e0693141dae5b5f1dc5c10625723d84666a
sedson <sedsonsteph@gmail.com>
2018-06-14 09:37:38 +0300

    a

--- application/views/maki-index.php
+++ application/views/maki-index.php
@@ -1,28 +1,4 @@
 	<section id="main-slider" class="no-margin">
-        <div class="carousel slide">      
-            <div class="carousel-inner">
-                <div class="item active" style="background-image: url(<?php echo base_url(); ?>assets/images/slider/bg1.jpg)">
-                    <div class="container">
-                        <div class="row slide-margin">
-                            <div class="col-sm-6">
-                                <div class="carousel-content">
-                                    <h2 class="animation animated-item-1">Bienvenue <span>Easy Job</span></h2>
-                                    <p class="animation animated-item-2">Trouver le <strong>JOB</strong> de votre reve avec nos services au pointe de la technologie et nos grands collaborateurs partout a Madagascar.</p>
-                                    <a class="btn-slide animation animated-item-3" href="#">Deposer mon CV</a>
-                                </div>
-                            </div>
-
-                            <div class="col-sm-6 hidden-xs animation animated-item-4">
-                                <div class="slider-img">
-                                    <img src="<?php echo base_url(); ?>assets/images/slider/img3.png" class="img-responsive">
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div><!--/.item-->             
-            </div><!--/.carousel-inner-->
-        </div><!--/.carousel-->
     </section><!--/#main-slider-->
 	
 	<div class="feature">
@@ -181,94 +157,14 @@
                     </div>
                     <div class="row">
                         <div class="col-md-offset-8 form-group ">
-                            <button type="submit" name="submit" class="btn btn-primary btn-lg" required="required">Envoyer CV</button>
+                            <button type="submit" name="submit" class="btn btn-primary btn-lg" required="required">Confirmer</button>
                         </div>
                     </div>
 					<?php echo form_close(); ?>
 				</div><!--/.row-->
-				<div class="col-md-3">
-					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms" >
-						<i class="fa fa-book"></i>
-						<h2>Easy Job E.Z</h2>
-						<p> L’utilisation de notre site vous épargne les préoccupations administratives</p>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms" >
-						<i class="fa fa-cloud"></i>
-						<h2>Dossiers</h2>
-						<p>Un depot assuré par nos partenaires.</p>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms" >
-						<i class="fa fa-laptop"></i>	
-						<h2>Entretiens en ligne</h2>
-						<p>On vous évite le déplacement, les entretiens se font en ligne.</p>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" >
-						<i class="fa fa-heart-o"></i>	
-						<h2>Easy Job Miles</h2>
-						<p>Gagner des miles à chaque depots</p>
-					</div>
-				</div>
+
+
 			</div>
 		</div>
 	</div>
-	
-	<div class="about">
-		<div class="container">
-			<h2 class="fadeInDown" data-wow-duration="1000ms" data-wow-delay="100ms" >Sur nous</h2>
-			<div class="col-md-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms" >
-				<img src="<?php echo base_url(); ?>assets/images/6.jpg" class="img-responsive"/>
-				<p>Plan'Air dessert 19 destinations sur l’ensemble de son réseau qui s’étend de Madagascar vers l’Océan Indien, et l’Europe.
-				</p>
-			</div>
-			<div class="col-md-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" >
-				<p>Plan'Air, transporteur international de premier plan.
-				<p>Figure incontournable du transport aérien dans la région Océan Indien, Plan'Air propose des offres étendues via des vols opérés en partage de codes avec ses partenaires.</p>
-				<p>Grâce au partenariat avec Air Mauritius, la destination Maurice est ainsi desservie jusqu’à 06 vols hebdomadaires entre Antananarivo et Port-Louis.</p>
-				<p>La Réunion est desservie quotidiennement entre Antananarivo et Saint-Denis conjointement avec Air Austral.</p>
-				<p>Le partenariat avec Air Seychelles a permis la reprise de la destination Seychelles.</p>
-			</div>
-		</div>
-	</div>
-
-	<section id="partner">
-        <div class="container">
-            <div class="center wow fadeInDown">
-                <h2>Nos Partenaires</h2>
-                <p>Les partenaires de la compagnie Plan'Air assurant votre expérience au cours des vols.</p>
-            </div>    
-
-            <div class="partners">
-                <ul>
-                    <li> <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms" src="<?php echo base_url(); ?>assets/images/partners/partner1.png"></a></li>
-                    <li> <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" src="<?php echo base_url(); ?>assets/images/partners/partner2.png"></a></li>
-                    <li> <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms" src="<?php echo base_url(); ?>assets/images/partners/partner3.png"></a></li>
-                    <li> <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms" src="<?php echo base_url(); ?>assets/images/partners/partner4.png"></a></li>
-                    <li> <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1500ms" src="<?php echo base_url(); ?>assets/images/partners/partner5.png"></a></li>
-                </ul>
-            </div>        
-        </div><!--/.container-->
-    </section><!--/#partner-->
-	
-	<section id="conatcat-info">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-8">
-                    <div class="media contact-info wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
-                        <div class="pull-left">
-                            <i class="fa fa-phone"></i>
-                        </div>
-                        <div class="media-body">
-                            <h2>Have a question or need a custom quote?</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div><!--/.container-->    
-    </section><!--/#conatcat-info-->
 
